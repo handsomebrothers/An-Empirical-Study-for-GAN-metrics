832acb4ed15fa47213cd60695bada271445f4e08
handsomebrothers <2805888621@qq.com>
2019-10-29 21:03:08 +0800

    Create infogan_mode_confusion.py

---
+++ IAM-Mode Confusion/mnist/infogan_mode_confusion.py
@@ -0,0 +1,22 @@
+#-*-coding:utf-8-*-
+import util
+import tensorflow as tf
+import numpy as np
+from keras.datasets import mnist
+import os
+MNIST_CLASSIFIER_FROZEN_GRAPH = '/home/ubuntu/soft/huangdengrong/GAN/Metrics-GAN/dataset/classify_mnist_graph_def.pb'
+INPUT_TENSOR = 'inputs:0'
+OUTPUT_TENSOR = 'logits:0'
+def get_mean_var(y_logit):
+    y_logit=np.abs(y_logit-0.1)
+    return np.mean(y_logit,axis=1)
+
+def get_possibility(images):
+    eval_images = tf.convert_to_tensor(images)
+    y_logits = util.mnist_logits(eval_images, MNIST_CLASSIFIER_FROZEN_GRAPH, INPUT_TENSOR, OUTPUT_TENSOR)
+    y_logits = tf.Session().run(tf.nn.softmax(y_logits))
+    # print(y_logits)
+    return y_logits
+if not os.path.isdir('saved_models_{}'.format('bgan')):
+    os.mkdir('saved_models_{}'.format('bgan'))
+f = open('saved_models_{}/log_collapse1.txt'.format('bgan'), mode='w')
